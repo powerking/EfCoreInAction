d10d4ab85d337461a6bcca1c741d0678cdb7d203
LeeWang <lee.wang@grapecity.com>
2022-08-26 14:26:38 +0800

    use non-static class

--- MyFirstEfCoreApp/Commands.cs
+++ MyFirstEfCoreApp/Commands.cs
@@ -11,179 +11,183 @@ using Microsoft.Extensions.Logging;
 
 namespace MyFirstEfCoreApp
 {
-    public static class Commands
-    {
+	internal class Commands : IDisposable
+	{
+		private AppDbContext _dbContext = new AppDbContext();
+		internal void ListAll()
+		{
+			//using (var _dbContext = new AppDbContext())              //#A
+			{
+				foreach (var book in _dbContext.Set<Book>().AsNoTracking() //#B
+						.Include(a => a.Author))                 //#C
+				{
+					var webUrl = book.Author.WebUrl ?? "- no web url given -";
+					Console.WriteLine($"{book.Title} by {book.Author.Name}");
+					Console.WriteLine("     Published on " +
+							$"{book.PublishedOn:dd-MMM-yyyy}. {webUrl}");
+				}
+			}
+		}
+		/**************************************************************
+		#A We create the application's DbContext through which all database accesses are done
+		#B This reads all the books. The AsNoTracking() says this is a read-only access
+		#C The include causes the Author information to be ‘eagerly’ loaded with each book. See chapter 2 for more on this
+		 * *************************************************************/
 
+		internal void ChangeWebUrl()
+		{
+			Console.Write("New Quantum Networking WebUrl > ");
+			var newWebUrl = Console.ReadLine();                   //#A
 
-        public static void ListAll()
-        {
-            using (var db = new AppDbContext())              //#A
-            {
-                foreach (var book in db.Set<Book>().AsNoTracking() //#B
-                    .Include(a => a.Author))                 //#C
-                {
-                    var webUrl = book.Author.WebUrl ?? "- no web url given -";
-                    Console.WriteLine($"{book.Title} by {book.Author.Name}");
-                    Console.WriteLine("     Published on " +
-                        $"{book.PublishedOn:dd-MMM-yyyy}. {webUrl}");
-                }
-            }
-        }
-        /**************************************************************
-        #A We create the application's DbContext through which all database accesses are done
-        #B This reads all the books. The AsNoTracking() says this is a read-only access
-        #C The include causes the Author information to be ‘eagerly’ loaded with each book. See chapter 2 for more on this
-         * *************************************************************/
+			//using (var _dbContext = new AppDbContext())
+			{
+				var book = _dbContext.Set<Book>()
+						.Include(a => a.Author)                        //#B
+						.Single(b => b.Title == "Quantum Networking"); //#C
 
-        public static void ChangeWebUrl()
-        {
-            Console.Write("New Quantum Networking WebUrl > ");
-            var newWebUrl = Console.ReadLine();                   //#A
+				book.Author.WebUrl = newWebUrl;                    //#D
+				_dbContext.SaveChanges();                                  //#E
+				Console.WriteLine("... SavedChanges called.");
+			}
 
-            using (var db = new AppDbContext())
-            {
-                var book = db.Set<Book>()
-                    .Include(a => a.Author)                        //#B
-                    .Single(b => b.Title == "Quantum Networking"); //#C
+			ListAll();                                             //#F
+		}
+		/**************************************************************
+		#A We read in from the console the new url
+		#B We make sure the author information is 'eager' loaded with the book
+		#C We select only the book with the title "Quantum Networking"
+		#D To update the database we simply change the data that was read in
+		#E The SaveChanges() tells EF Core to check for any changes to the data that has been read in and write out those changes to the database
+		#F Finally we list all the book information
 
-                book.Author.WebUrl = newWebUrl;                    //#D
-                db.SaveChanges();                                  //#E
-                Console.WriteLine("... SavedChanges called.");
-            }
+		 * ************************************************************/
 
-            ListAll();                                             //#F
-        }
-        /**************************************************************
-        #A We read in from the console the new url
-        #B We make sure the author information is 'eager' loaded with the book
-        #C We select only the book with the title "Quantum Networking"
-        #D To update the database we simply change the data that was read in
-        #E The SaveChanges() tells EF Core to check for any changes to the data that has been read in and write out those changes to the database
-        #F Finally we list all the book information
+		internal void ListAllWithLogs()
+		{
+			var logs = new List<string>();
+			//using (var _dbContext = new AppDbContext())
+			{
+				var serviceProvider = _dbContext.GetInfrastructure();
+				var loggerFactory = (ILoggerFactory)serviceProvider.GetService(typeof(ILoggerFactory));
+				loggerFactory.AddProvider(new MyLoggerProvider(logs));
 
-         * ************************************************************/
+				foreach (var book in
+						_dbContext.Set<Book>().AsNoTracking()
+						.Include(a => a.Author))
+				{
+					var webUrl = book.Author.WebUrl == null
+							? "- no web url given -"
+							: book.Author.WebUrl;
+					Console.WriteLine(
+							$"{book.Title} by {book.Author.Name}");
+					Console.WriteLine("     " +
+							$"Published on {book.PublishedOn:dd-MMM-yyyy}" +
+							$". {webUrl}");
+				}
+			}
+			Console.WriteLine("---------- LOGS ------------------");
+			foreach (var log in logs)
+			{
+				Console.WriteLine(log);
+			}
+		}
 
-        public static void ListAllWithLogs()
-        {
-            var logs = new List<string>();
-            using (var db = new AppDbContext())
-            {
-                var serviceProvider = db.GetInfrastructure();
-                var loggerFactory = (ILoggerFactory)serviceProvider.GetService(typeof(ILoggerFactory));
-                loggerFactory.AddProvider(new MyLoggerProvider(logs));
+		internal void ChangeWebUrlWithLogs()
+		{
+			var logs = new List<string>();
+			Console.Write("New Quantum Networking WebUrl > ");
+			var newWebUrl = Console.ReadLine();
 
-                foreach (var book in
-                    db.Set<Book>().AsNoTracking()
-                    .Include(a => a.Author))
-                {
-                    var webUrl = book.Author.WebUrl == null
-                        ? "- no web url given -"
-                        : book.Author.WebUrl;
-                    Console.WriteLine(
-                        $"{book.Title} by {book.Author.Name}");
-                    Console.WriteLine("     " +
-                        $"Published on {book.PublishedOn:dd-MMM-yyyy}" +
-                        $". {webUrl}");
-                }
-            }
-            Console.WriteLine("---------- LOGS ------------------");
-            foreach (var log in logs)
-            {
-                Console.WriteLine(log);
-            }
-        }
+			//using (var _dbContext = new AppDbContext())
+			{
+				var serviceProvider = _dbContext.GetInfrastructure();
+				var loggerFactory = (ILoggerFactory)serviceProvider.GetService(typeof(ILoggerFactory));
+				loggerFactory.AddProvider(new MyLoggerProvider(logs));
 
-        public static void ChangeWebUrlWithLogs()
-        {
-            var logs = new List<string>();
-            Console.Write("New Quantum Networking WebUrl > ");
-            var newWebUrl = Console.ReadLine();
+				var book = _dbContext.Set<Book>()
+						.Include(a => a.Author)
+						.Single(b => b.Title == "Quantum Networking");
+				book.Author.WebUrl = newWebUrl;
+				_dbContext.SaveChanges();
+				Console.Write("... SavedChanges called.");
+			}
+			Console.WriteLine("---------- LOGS ------------------");
+			foreach (var log in logs)
+			{
+				Console.WriteLine(log);
+			}
+		}
 
-            using (var db = new AppDbContext())
-            {
-                var serviceProvider = db.GetInfrastructure();
-                var loggerFactory = (ILoggerFactory)serviceProvider.GetService(typeof(ILoggerFactory));
-                loggerFactory.AddProvider(new MyLoggerProvider(logs));
+		/// <summary>
+		/// This will wipe and create a new database - which takes some time
+		/// </summary>
+		/// <param name="onlyIfNoDatabase">If true it will not do anything if the database exists</param>
+		/// <returns>returns true if database database was created</returns>
+		internal bool WipeCreateSeed(bool onlyIfNoDatabase)
+		{
+			//using (var _dbContext = new AppDbContext())
+			{
+				if (onlyIfNoDatabase && (_dbContext.GetService<IDatabaseCreator>() as RelationalDatabaseCreator).Exists())
+					return false;
 
-                var book = db.Set<Book>()
-                    .Include(a => a.Author)
-                    .Single(b => b.Title == "Quantum Networking");
-                book.Author.WebUrl = newWebUrl;
-                db.SaveChanges();
-                Console.Write("... SavedChanges called.");
-            }
-            Console.WriteLine("---------- LOGS ------------------");
-            foreach (var log in logs)
-            {
-                Console.WriteLine(log);
-            }
-        }
+				_dbContext.Database.EnsureDeleted();
+				_dbContext.Database.EnsureCreated();
+				if (!_dbContext.Set<Book>().Any())
+				{
+					WriteTestData(_dbContext);
+					Console.WriteLine("Seeded database");
+				}
+			}
+			return true;
+		}
 
-        /// <summary>
-        /// This will wipe and create a new database - which takes some time
-        /// </summary>
-        /// <param name="onlyIfNoDatabase">If true it will not do anything if the database exists</param>
-        /// <returns>returns true if database database was created</returns>
-        public static bool WipeCreateSeed(bool onlyIfNoDatabase)
-        {
-            using (var db = new AppDbContext())
-            {
-                if (onlyIfNoDatabase && (db.GetService<IDatabaseCreator>() as RelationalDatabaseCreator).Exists())
-                    return false;
+		private void WriteTestData(AppDbContext _dbContext)
+		{
+			var martinFowler = new Author
+			{
+				Name = "Martin Fowler",
+				WebUrl = "http://martinfowler.com/"
+			};
 
-                db.Database.EnsureDeleted();
-                db.Database.EnsureCreated();
-                if (!db.Set<Book>().Any())
-                {
-                    WriteTestData(db);
-                    Console.WriteLine("Seeded database");
-                }
-            }
-            return true;
-        }
+			var books = new List<Book>
+						{
+								new Book
+								{
+										Title = "Refactoring",
+										Description = "Improving the design of existing code",
+										PublishedOn = new DateTime(1999, 7, 8),
+										Author = martinFowler
+								},
+								new Book
+								{
+										Title = "Patterns of Enterprise Application Architecture",
+										Description = "Written in direct response to the stiff challenges",
+										PublishedOn = new DateTime(2002, 11, 15),
+										Author = martinFowler
+								},
+								new Book
+								{
+										Title = "Domain-Driven Design",
+										Description = "Linking business needs to software design",
+										PublishedOn = new DateTime(2003, 8, 30),
+										Author = new Author { Name = "Eric Evans", WebUrl = "http://domainlanguage.com/"}
+								},
+								new Book
+								{
+										Title = "Quantum Networking",
+										Description = "Entangled quantum networking provides faster-than-light data communications",
+										PublishedOn = new DateTime(2057, 1, 1),
+										Author = new Author { Name = "Future Person"}
+								}
+						};
 
-        public static void WriteTestData(this AppDbContext db)
-        {
-            var martinFowler = new Author
-            {
-                Name = "Martin Fowler",
-                WebUrl = "http://martinfowler.com/"
-            };
+			_dbContext.Set<Book>().AddRange(books);
+			_dbContext.SaveChanges();
+		}
 
-            var books = new List<Book>
-            {
-                new Book
-                {
-                    Title = "Refactoring",
-                    Description = "Improving the design of existing code",
-                    PublishedOn = new DateTime(1999, 7, 8),
-                    Author = martinFowler
-                },
-                new Book
-                {
-                    Title = "Patterns of Enterprise Application Architecture",
-                    Description = "Written in direct response to the stiff challenges",
-                    PublishedOn = new DateTime(2002, 11, 15),
-                    Author = martinFowler
-                },
-                new Book
-                {
-                    Title = "Domain-Driven Design",
-                    Description = "Linking business needs to software design",
-                    PublishedOn = new DateTime(2003, 8, 30),
-                    Author = new Author { Name = "Eric Evans", WebUrl = "http://domainlanguage.com/"}
-                },
-                new Book
-                {
-                    Title = "Quantum Networking",
-                    Description = "Entangled quantum networking provides faster-than-light data communications",
-                    PublishedOn = new DateTime(2057, 1, 1),
-                    Author = new Author { Name = "Future Person"}
-                }
-            };
-
-            db.Set<Book>().AddRange(books);
-            db.SaveChanges();
-        }
-    }
+		public void Dispose()
+		{
+			_dbContext.Dispose();
+		}
+	}
 }
--- MyFirstEfCoreApp/Program.cs
+++ MyFirstEfCoreApp/Program.cs
@@ -5,46 +5,54 @@ using System;
 
 namespace MyFirstEfCoreApp
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            //Console.WriteLine(
-            //    "Commands: l (list), u (change url), r (resetDb) and e (exit) - add -l to first two for logs");
-            Console.Write(
-                "Checking if database exists... ");
-            Console.WriteLine(Commands.WipeCreateSeed(true) ? "created database and seeded it." : "it exists.");
-            do
-            {
-                Console.WriteLine(
-                        "Commands: l (list), u (change url), r (resetDb) and e (exit) - add -l to first two for logs");
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			using (Commands commands = new Commands())
+			{
+				Console.Write(
+									"Checking if database exists... ");
+				Console.WriteLine(commands.WipeCreateSeed(true) ? "created database and seeded it." : "it exists.");
+				
+				ShowMeun(commands);
+			}
 
-                Console.Write("> ");
-                var command = Console.ReadLine();
-                switch (command)
-                {
-                    case "l":
-                        Commands.ListAll();
-                        break;
-                    case "u":
-                        Commands.ChangeWebUrl();
-                        break;
-                    case "l -l":
-                        Commands.ListAllWithLogs();
-                        break;
-                    case "u -l":
-                        Commands.ChangeWebUrlWithLogs();
-                        break;
-                    case "r":
-                        Commands.WipeCreateSeed(false);
-                        break;
-                    case "e":
-                        return;
-                    default: 
-                        Console.WriteLine("Unknown command.");
-                        break;
-                }
-            } while (true);
-        }
-    }
+		}
+
+		private static void ShowMeun(Commands Commands)
+		{
+			do
+			{
+				Console.WriteLine(
+								"Commands: l (list), u (change url), r (resetDb) and e (exit) - add -l to first two for logs");
+
+				Console.Write("> ");
+				var cmdText = Console.ReadLine();
+				switch (cmdText)
+				{
+					case "l":
+						Commands.ListAll();
+						break;
+					case "u":
+						Commands.ChangeWebUrl();
+						break;
+					case "l -l":
+						Commands.ListAllWithLogs();
+						break;
+					case "u -l":
+						Commands.ChangeWebUrlWithLogs();
+						break;
+					case "r":
+						Commands.WipeCreateSeed(false);
+						break;
+					case "e":
+						return;
+					default:
+						Console.WriteLine("Unknown command.");
+						break;
+				}
+			} while (true);
+		}
+	}
 }
